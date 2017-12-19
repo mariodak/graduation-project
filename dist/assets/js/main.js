52b07508e4208ddec6a8413fca6f447bbca81e19
Mario Dak <e.mariodak@gmail.com>
2017-12-19 01:37:17 +0100

    fixed user scalable, + safari

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // Main JS File
-
-
+document.addEventListener('touchmove', function (event) {
+    event = event.originalEvent || event;
+    if (event.scale !== 1) {
+        event.preventDefault();
+    }
+}, false);
 // Menu Toggle
 
 $('#active').click(function () {
